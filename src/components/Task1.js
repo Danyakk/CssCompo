0d5a387650f3fd0b9e0240fe0fa0fd80f37bd67f
Danyakk <kuhar1357@gmail,com>
2026-04-07 21:09:45 +0300

    12

--- src/components/Task1.js
+++ src/components/Task1.js
@@ -1,28 +1,30 @@
 
-export  function Home(){
+export function Home() {
     return <div className="home-div">
-        <div claaName="image">
-            <img src="iphone-17.png"/>
+        <div className="image">
+            <img src="iphone-17.png" />
         </div>
-        <div claaName="text">
-            <p>saf</p>
-            <p>sadf</p>
-            <p>sadf</p>
+        <div className="text">
+            <h1 className="product-title">iPhone 17 256/8 GB</h1>
+            <p className="product-description">
+                Experience the new iPhone 17 in a stunning range of colors:
+                black, white, blue, green, and purple.
+            </p>
         </div>
     </div>
 }
-export  function About(){
+export function About() {
     return <h2>About</h2>;
 }
-export  function Products(){
+export function Products() {
     return <h2>Products</h2>;
 }
-export  function Contacts(){
+export function Contacts() {
     return <h2>Contacts</h2>;
 }
-export  function Info(){
+export function Info() {
     return <h2>Info</h2>;
 }
-export function NotFound(){
+export function NotFound() {
     return <h2>Ресурс не найден 404</h2>;
 }
